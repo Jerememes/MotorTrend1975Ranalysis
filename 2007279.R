0a53467bed774236663e5e565481823fc154f15e
Jeremy R <85578952+Jerememes@users.noreply.github.com>
2022-12-01 22:36:29 -0500

    part b finished

--- 2007279.R
+++ 2007279.R
@@ -23,38 +23,90 @@ qqnorm(mondata$weight)
 qqline(mondata$weight)
 shapiro.test(mondata$weight)
 
+library(distributions3)
 n <- length(mondata)
 T_9 <- StudentsT(df = n - 1)
 mesures = data.frame(mondata=c("mpg", "displacement","Weight"),
                      Moyenne=NA, Q1=NA, Mediane=NA, Q3=NA, Écarttype=NA, ErreurType=NA,
                      IntervalleDeConfiancePourLaMoyenneBorneInférieure =NA,
                      IntervalleDeConfiancePourLaMoyenneBorneSupérieure =NA)
-mesures
 mesures$Moyenne    = sapply(1:3, function(i) mean(mondata[,i]))
-mesures
 mesures$Mediane = sapply(1:3, function(i) median(mondata[,i]))
-mesures
 mesures[1, c("Q1", "Q3")] = quantile(mondata$mpg, probs = c(0.25,0.75))
-mesures
 mesures[2, c("Q1", "Q3")] = quantile(mondata$displacement, probs = c(0.25,0.75))
-mesures
 mesures[3, c("Q1", "Q3")] = quantile(mondata$weight, probs = c(0.25,0.75))
-mesures
 mesures$Écarttype  = sapply(1:3, function(i) sd(mondata[,i]))
-mesures
-mesures$ErreurType = sapply(1:3, function(i) sqrt(sum((mondata[,i]-mean(mondata[,i]))^2/(length(mondata[,i])-1)))
-                                           / sqrt(length(mondata[,i])))
-mesures
-mesures$IntervalleDeConfiancePourLaMoyenneBorneInférieure = sapply(1:3, function(i) mean(mondata[,i]) 
-                                                                                    + quantile(T_9, 0.95/ 2) 
-                                                                                    * sd(mondata[,i]) / sqrt(n))
-mesures
-mesures$IntervalleDeConfiancePourLaMoyenneBorneSupérieure = sapply(1:3, function(i) mean(mondata[,i]) 
-                                                                        + quantile(T_9, 1 - 0.95/ 2) 
-                                                                        * sd(mondata[,i]) / sqrt(n))
+mesures$ErreurType = 
+  sapply(1:3, 
+    function(i) sqrt(
+      sum(
+        (mondata[,i]-mean(mondata[,i]))^2/(length(mondata[,i])-1)
+      )
+    )
+    / sqrt(length(mondata[,i]))
+  )
+mesures$IntervalleDeConfiancePourLaMoyenneBorneInférieure = 
+  sapply(1:3, function(i)
+    mean(mondata[,i]) 
+  + quantile(T_9, 0.95/ 2) 
+  * sd(mondata[,i]) / sqrt(n)
+  )
+mesures$IntervalleDeConfiancePourLaMoyenneBorneSupérieure = 
+  sapply(1:3, function(i)
+    mean(mondata[,i])
+  + quantile(T_9, 1 - 0.95/ 2)
+  * sd(mondata[,i]) / sqrt(n)
+  )
 mesures
 
-# Un test d’hypothèses sur l’égalité des variances des deux groupes.
-# Un test d’hypothèses sur l’égalité des moyennes des deux groupes.
+# c)
+
+library(magrittr)
+library(ggplot2)
+multi <- data.frame(
+  type = c( rep("Origin 0"), rep("Origin 1")),
+  value = c( mondata$mpg[mondata$origin=="0"], mondata$mpg[mondata$origin=="1"])
+)
+multi %>%
+  ggplot(aes(x=value, fill=type)) +
+  geom_histogram(color="#e9ecef",
+                 alpha=0.6,
+                 position = 'identity') +
+  scale_fill_manual(values=c("#69b3a2", "#404080")) +
+  labs(fill="") + ggtitle('mpg')
+
+boxplot(mondata$mpg[mondata$origin=="0"]~mondata$mpg[mondata$origin=="1"],
+        data=mondata,
+        col=c("lightpink","lightblue"),
+        horizontal=TRUE,
+        notch=TRUE,
+        main=paste("mpg de ",nrow(mondata)," de véhicules"),
+        ylab="Origine",
+        las=1)
+
+
+multi <- data.frame(
+  type = c( rep("Origin 0"), rep("Origin 1")),
+  value = c( mondata$displacement[mondata$origin=="0"], mondata$displacement[mondata$origin=="1"])
+)
+multi %>%
+  ggplot(aes(x=value, fill=type)) +
+  geom_histogram(color="#e9ecef",
+                 alpha=0.6,
+                 position = 'identity') +
+  scale_fill_manual(values=c("#69b3a2", "#404080")) +
+  labs(fill="") + ggtitle('displacement')
+multi <- data.frame(
+  type = c( rep("Origin 0"), rep("Origin 1")),
+  value = c( mondata$weight[mondata$origin=="0"], mondata$weight[mondata$origin=="1"])
+)
+multi %>%
+  ggplot(aes(x=value, fill=type)) +
+  geom_histogram(color="#e9ecef",
+                 alpha=0.6,
+                 position = 'identity') +
+  scale_fill_manual(values=c("#69b3a2", "#404080")) +
+  labs(fill="") + ggtitle('weight')
+
 
 
